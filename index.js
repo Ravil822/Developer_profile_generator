59e267608cd3b3e124d1be8a4fe64c0749fc9aaa
RAvil <russian82@mail.com>
2020-01-18 22:58:14 -0500

    add html and make call to api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let axios = require("axios");
 inquirer.prompt([
     {
         type: "input",
-        name: "userName",
+        name: "gitUser",
         message: "Please, enter a GitHub username"
     },
     {
@@ -14,9 +14,38 @@ inquirer.prompt([
         message: "What color do you like as background color?",
         name: "color",
         choices: [
-          "blue",
-          "yellow",
-          "green"
+            "blue",
+            "yellow",
+            "green"
         ]
     }
-]);
+]).then(answers => {
+    let color = answers.color
+    const queryUrl = `https://api.github.com/users/${answers.gitUser}`;
+    const queryUrlStar = `https://api.github.com/users/${answers.gitUser}/repos?per_page=100`;
+    axios.get(queryUrl).then(function (res) {
+        let name = res.data.name;
+        let profileImage = res.data.avatar_url;
+        let userCompany = res.data.company;
+        let repos = res.data.public_repos;
+        let followers = res.data.followers;
+        let following = res.data.following;
+        let user = res.data.html_url;
+        let location = res.data.location;
+        let bio = res.data.bio;
+        console.log(userCompany);
+        console.log(name);
+        console.log(profileImage);
+        console.log(location)
+        console.log(user)    
+    })
+    console.log(color);
+    let stars =  axios.get(queryUrlStar).then(function (resStar) {
+        let dat = resStar.data[0].stargazers_count;
+    })
+
+
+
+
+});
+
